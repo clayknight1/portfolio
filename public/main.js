129062f15d90c2e68506120ba0eaaeaa23e1efcb
Clay Knight <clayknight@rocketmail.com>
2017-12-01 15:07:12 -0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   public/grid.css
    	modified:   public/main.js
    	modified:   public/styles.css
    	modified:   server.js
    	modified:   views/index.ejs
    	modified:   views/thanks.ejs
    modified files

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 function hamburgerHandler() {
-    document.getElementById('side-nav').style.display = 'block';
+    document.getElementById('side-nav').style.right = '0';
+}
+
+function closeNavHandler() {
+    document.getElementById('side-nav').style.right = '-400px';
 }
 
 document.getElementById('hamburger').onclick = hamburgerHandler;
+document.getElementById('close-nav').onclick = closeNavHandler;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const app = express();
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'));
-// app.use(express.static(__dirname));
+app.set('port', (process.env.PORT || 8080));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -29,16 +32,22 @@ app.get('/contact', (req, res) => {
 
 app.post('/thanks', (req, res) => {
   var userInfo = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName
-    // name: req.body.name,
-    // email: req.body.email,
-    // message: req.body.message
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
   }
-  res.render('thanks', {contact:userInfo})
+  
+  client.messages
+    .create({
+      to: '+14155318563',
+      from: '+14158959704',
+      body: `Message from: ${userInfo.name}` + '\n' + `Email: ${userInfo.email}` + '\n' + `Message: ${userInfo.message}`,
+    })
+    .then((message) => console.log(message.sid));
+  res.render('thanks', {userInfo: req.body})
 });
 
 
-app.listen(8080, ()=>{
-  console.log('listening at http://localhost:8080')
-})
+app.listen(app.get('port'), () => {
+  console.log('listening at http://localhost:' + app.get('port'));
+});
